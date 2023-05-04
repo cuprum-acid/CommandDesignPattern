73dd9da596484bc5f82dec5eb8e7a857dc28a8a0
cuprum-acid <jony.zh@yandex.ru>
2023-05-04 17:11:38 +0300

    Initial commit

---
+++ src/Command.java
@@ -0,0 +1,22 @@
+/**
+ * Command is the base class for all commands.
+ * It defines the methods that all commands should implement.
+ */
+public abstract class Command {
+    public Editor editor;
+    private String backup;
+
+    Command(Editor editor) {
+        this.editor = editor;
+    }
+
+    void backup() {
+        backup = editor.textField.getText();
+    }
+
+    public void undo() {
+        editor.textField.setText(backup);
+    }
+
+    public abstract boolean execute();
+}
---
+++ src/Editor.java
@@ -0,0 +1,80 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Editor is an example of text editor. Uses Command pattern and Java GUI.
+ * @see <a href="https://refactoring.guru/design-patterns/command/java/example#example-0">Command in Java</a>
+ */
+public class Editor {
+    public JTextArea textField;
+    public String clipboard;
+    private CommandHistory history = new CommandHistory();
+
+    public void init() {
+        JFrame frame = new JFrame("Text editor (special for Marko Pezer <3)");
+        JPanel content = new JPanel();
+        frame.setContentPane(content);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+        textField = new JTextArea();
+        textField.setLineWrap(true);
+        content.add(textField);
+        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JButton ctrlC = new JButton("Ctrl+C (copy)");
+        JButton ctrlX = new JButton("Ctrl+X (cut)");
+        JButton ctrlV = new JButton("Ctrl+V (paste)");
+        JButton ctrlZ = new JButton("Ctrl+Z (undo)");
+        Editor editor = this;
+        ctrlC.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                executeCommand(new CopyCommand(editor));
+            }
+        });
+        ctrlX.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                executeCommand(new CutCommand(editor));
+            }
+        });
+        ctrlV.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                executeCommand(new PasteCommand(editor));
+            }
+        });
+        ctrlZ.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                undo();
+            }
+        });
+        buttons.add(ctrlC);
+        buttons.add(ctrlX);
+        buttons.add(ctrlV);
+        buttons.add(ctrlZ);
+        content.add(buttons);
+        frame.setSize(450, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void executeCommand(Command command) {
+        if (command.execute()) {
+            history.push(command);
+        }
+    }
+
+    private void undo() {
+        if (history.isEmpty()) return;
+
+        Command command = history.pop();
+        if (command != null) {
+            command.undo();
+        }
+    }
+}
